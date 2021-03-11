28421e2c17208091205751876dfb1825a1a1d072
bumblePrime <57355152+bumblePrime@users.noreply.github.com>
2021-03-11 19:52:46 +0530

    Credentials
    Credentials you will need to access telegram API and search database.

---
+++ creds.py
@@ -0,0 +1,6 @@
+bot_token="Your API Token"
+bot_username="Your bot username"
+URL="Heroku URL for the app"
+database='Your database name'
+table='table to be searched'
+#PS: you can search multiple tables at once using UNION function in MySQL
